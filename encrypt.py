58afcb57c803cbde5631e9dd5ead3f28cca903ea
John Q. <johnequarmor@gmail.com>
2026-01-31 07:27:39 +0000

    Restored "encrypt.py" file.

---
+++ encrypt.py
@@ -0,0 +1,27 @@
+import os
+from cryptography.fernet import Fernet
+
+os.chdir("target_folder")
+
+files = []
+key = Fernet.generate_key()
+
+for file in os.listdir():
+	if file == "encrypt.py" or file == "thekey.key" or file == "decrypt.py":
+		continue
+	if os.path.isfile(file):
+		files.append(file)
+
+print(files)
+
+with open("thekey.key", "wb") as thekey:
+	thekey.write(key)
+
+for file in files:
+	with open(file, "rb") as thefile:
+		contents = thefile.read()
+	contents_encrypted = Fernet(key).encrypt(contents)
+	with open(file, "wb") as thefile:
+		thefile.write(contents_encrypted)
+
+print("All files within the target folder have been successfully encrypted!")
