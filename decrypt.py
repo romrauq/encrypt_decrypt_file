8369fa1f75a42e8e1643cc2aaa50a74aca0c77d8
John Q. <johnequarmor@gmail.com>
2026-02-23 16:35:11 +0000

    Changed target directory name.

--- decrypt.py
+++ decrypt.py
@@ -1,26 +1,26 @@
 import os
 from cryptography.fernet import Fernet
 
-os.chdir("target_folder")
+os.chdir("target_dir")
 
 files = []
 
 for file in os.listdir():
-	if file == "encrypt.py" or file == "thekey.key" or file == "decrypt.py":
-		continue
-	if os.path.isfile(file):
-		files.append(file) 
+    if file == "encrypt.py" or file == "thekey.key" or file == "decrypt.py":
+        continue
+    if os.path.isfile(file):
+        files.append(file)
 
 print(files)
 
 with open("thekey.key", "rb") as key:
-	secretKey = key.read()
+    secretKey = key.read()
 
 for file in files:
-	with open(file, "rb") as thefile:
-		contents = thefile.read()
-	contents_decrypted = Fernet(secretKey).decrypt(contents)
-	with open(file, "wb") as thefile:
-		thefile.write(contents_decrypted)
+    with open(file, "rb") as thefile:
+        contents = thefile.read()
+    contents_decrypted = Fernet(secretKey).decrypt(contents)
+    with open(file, "wb") as thefile:
+        thefile.write(contents_decrypted)
 
 print("All files within the target folder have been successfully decrypted!")
